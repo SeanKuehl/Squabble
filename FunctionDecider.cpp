dfa30acd2401b63587a972e046ccd237d9376bee
SeanKuehl <teddscottfelt@gmail.com>
2023-04-20 15:16:19 -0400

    added subtract command

--- FunctionDecider.cpp
+++ FunctionDecider.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#include <string.h>
 #include "Variable.h"
 
 using namespace std;
@@ -26,6 +27,11 @@ void AddStringVariables(string firstVariable, string secondVariable);
 void AddIntegerVariables(string firstVariable, string secondVariable);
 void AddCharacterVariables(string firstVariable, string secondVariable);
 void AddDoubleVariables(string firstVariable, string secondVariable);
+
+void SubtractStringVariables(string firstVariable, string secondVariable);
+void SubtractIntegerVariables(string firstVariable, string secondVariable);
+void SubtractCharacterVariables(string firstVariable, string secondVariable);
+void SubtractDoubleVariables(string firstVariable, string secondVariable);
 
 
 void DecideCommand(vector<vector<string>> lines){
@@ -86,11 +92,136 @@ void DecideCommand(vector<vector<string>> lines){
             else if (StringToUpper(thisLine.at(0)) == "ADDD"){
                 AddDoubleVariables(thisLine.at(1), thisLine.at(2));
             }
+            else if (StringToUpper(thisLine.at(0)) == "SUBS"){
+                SubtractStringVariables(thisLine.at(1), thisLine.at(2));
+            }
+            else if (StringToUpper(thisLine.at(0)) == "SUBI"){
+                SubtractIntegerVariables(thisLine.at(1), thisLine.at(2));
+            }
+            else if (StringToUpper(thisLine.at(0)) == "SUBC"){
+                SubtractCharacterVariables(thisLine.at(1), thisLine.at(2));
+            }
+            else if (StringToUpper(thisLine.at(0)) == "SUBD"){
+                SubtractDoubleVariables(thisLine.at(1), thisLine.at(2));
+            }
 
 
     }
 }
 
+void SubtractStringVariables(string firstVariable, string secondVariable){
+    //replaces last x characters of first string with second variable
+    //where x is the length of the second variable
+    //if both variables are the same size, produce empty string
+    //if second variable is longer, keep characters in second variable
+    //after x where x is the length of the first variable
+    string resultingString = "";
+
+    for (int i = 0; i<userVariables.size();i++){
+        if (userVariables.at(i).GetName() == firstVariable){
+            resultingString += userVariables.at(i).GetStringValue();
+        }
+    }
+
+    for (int i = 0; i<userVariables.size();i++){
+        if (userVariables.at(i).GetName() == secondVariable){
+            string secondString = userVariables.at(i).GetStringValue();
+
+            if (secondString.size() > resultingString.size()){
+                resultingString = secondString.substr(resultingString.size()+1, resultingString.size());
+
+            }
+            else if (secondString.size() == resultingString.size()){
+                resultingString = "";
+            }
+            else if (secondString.size() < resultingString.size()){
+                int originalSize = resultingString.size();
+                resultingString = resultingString.substr(0,resultingString.size()-secondString.size());
+                resultingString += secondString;
+
+            }
+
+        }
+    }
+
+
+    for (int i = 0; i<userVariables.size();i++){
+        if (userVariables.at(i).GetName() == firstVariable){
+            userVariables.at(i).SetStringValue(resultingString);
+        }
+    }
+
+}
+
+void SubtractIntegerVariables(string firstVariable, string secondVariable){
+
+    int resultingValue = 0;
+
+    for (int i = 0; i<userVariables.size();i++){
+        if (userVariables.at(i).GetName() == firstVariable){
+            resultingValue = userVariables.at(i).GetIntValue();
+        }
+    }
+
+    for (int i = 0; i<userVariables.size();i++){
+        if (userVariables.at(i).GetName() == secondVariable){
+            resultingValue -= userVariables.at(i).GetIntValue();
+        }
+    }
+
+    for (int i = 0; i<userVariables.size();i++){
+        if (userVariables.at(i).GetName() == firstVariable){
+            userVariables.at(i).SetIntValue(resultingValue);
+        }
+    }
+
+
+}
+
+void SubtractCharacterVariables(string firstVariable, string secondVariable){
+    int resultingChar = 0;
+
+    for (int i = 0; i<userVariables.size();i++){
+        if (userVariables.at(i).GetName() == firstVariable){
+            resultingChar += (int)(userVariables.at(i).GetCharValue());
+        }
+    }
+
+    for (int i = 0; i<userVariables.size();i++){
+        if (userVariables.at(i).GetName() == secondVariable){
+            resultingChar += (int)(userVariables.at(i).GetCharValue());
+        }
+    }
+
+    for (int i = 0; i<userVariables.size();i++){
+        if (userVariables.at(i).GetName() == firstVariable){
+            userVariables.at(i).SetCharValue((char)(resultingChar));
+        }
+    }
+
+}
+
+void SubtractDoubleVariables(string firstVariable, string secondVariable){
+    double resultingDouble = 0.0;
+
+    for (int i = 0; i<userVariables.size();i++){
+        if (userVariables.at(i).GetName() == firstVariable){
+            resultingDouble += userVariables.at(i).GetDoubleValue();
+        }
+    }
+
+    for (int i = 0; i<userVariables.size();i++){
+        if (userVariables.at(i).GetName() == secondVariable){
+            resultingDouble += userVariables.at(i).GetDoubleValue();
+        }
+    }
+
+    for (int i = 0; i<userVariables.size();i++){
+        if (userVariables.at(i).GetName() == firstVariable){
+            userVariables.at(i).SetDoubleValue(resultingDouble);
+        }
+    }
+}
 
 void AddStringVariables(string firstVariable, string secondVariable){
     string resultingString = "";
